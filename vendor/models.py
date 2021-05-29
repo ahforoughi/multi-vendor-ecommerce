a6c71eec03a6f5062ed21be32e8ded68333e1600
Amirhossein Foroughi <ah.foroughi98@gmail.com>
2021-05-29 18:10:55 +0430

    vendor model registered

--- vendor/models.py
+++ vendor/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Vendor(models.Model):
     name =  models.CharField(max_length=255)
-    
+    creation_date = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(User, related_name='vendor', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ['name']
