ebde502cc27a0cb1b2d4f9bf9dbed245b98309ca
Amirhossein Foroughi <amirhosseinforoughi2@gmail.com>
2021-05-31 17:50:29 +0430

    product and category models and apis are completed

--- chenaar/settings.py
+++ chenaar/settings.py
@@ -37,13 +37,28 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders', 
+    'djoser',
+
     'shop', 
-    'vendor'
+    'vendor',
+    'product'
+
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,34 @@
+import vendor
+from django import forms
 from django.shortcuts import render
+from django.http import JsonResponse
+from json import JSONEncoder
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Vendor
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def vendor_register(request):
-    pass
+
+    if request.method == 'POST': 
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            vendor = Vendor.objects.create(
+                name=user.name, 
+                created_by=user
+            )
+            print (f"{vendor} submitted!")
+        else:
+            print ("Form is not valid")
+            #print (form)
+    else: 
+        form = UserCreationForm()
+        print("empty form created")
+    
+    return JsonResponse({
+        'status': 'ok',
+    }, encoder=JSONEncoder)
