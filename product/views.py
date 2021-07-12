fbf67cce93344a6ea5212a2e2318c2b5931b012f
Amirhossein Foroughi <amirhosseinforoughi2@gmail.com>
2021-07-12 19:55:15 +0430

    search and category view added

--- product/views.py
+++ product/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import logging
 from .models import Product, Category
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 
 logger = logging.getLogger('django')
 
@@ -28,7 +28,31 @@ class ProductDetail(APIView):
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)        
-        logger.info(f'product {product} found!!!!')
+        logger.info(f'product {product} found')
         return Response(serializer.data)
 
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404        
 
+    def get(self, request, category_slug):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)        
+        logger.info(f'category {category} found!')
+        return Response(serializer.data)
+
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+#TODO: figure out how to test this with postman 
+    if query:
+        print(query)
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else:
+        return Response({"products": []})
